9abebaff555553de415e38bff7fe2741330984f5
gschnabel <40870991+gschnabel@users.noreply.github.com>
2023-10-18 22:10:14 +0200

    remove unused var mxm

--- source/DATP.py
+++ source/DATP.py
@@ -469,15 +469,15 @@ def reduce_data():
         # NOTE: computed goto of fortran replaced
         #       by if-else statements
         if xp.NT in (1, 2):
-            E11, E22, mxm, mxm1 = deal_with_CS_and_CS_SHAPE(xp, NOD, ER, T, EQ, Q)
+            E11, E22, _, mxm1 = deal_with_CS_and_CS_SHAPE(xp, NOD, ER, T, EQ, Q)
         elif xp.NT in (3, 4):
-            E11, E22, mxm, mxm1, _, _, _, _, _, _ = deal_with_RATIO_and_RATIO_SHAPE(xp, NOD, ER, T, EQ, Q)
+            E11, E22, _, mxm1, _, _, _, _, _, _ = deal_with_RATIO_and_RATIO_SHAPE(xp, NOD, ER, T, EQ, Q)
         elif xp.NT in (5, 8):
-            E11, E22, mxm, mxm1, _, _, _, _, _, _ = deal_with_SUM_and_SHAPE_OF_SUM(xp, NOD, ER, T, EQ, Q)
+            E11, E22, _, mxm1, _, _, _, _, _, _ = deal_with_SUM_and_SHAPE_OF_SUM(xp, NOD, ER, T, EQ, Q)
         elif xp.NT == 6:
             goto .lbl14
         elif xp.NT in (7, 9):
-            E11, E22, mxm, mxm1, _, _, _, _, _, _ = deal_with_CS_VS_SUM_PLUS_SHAPE(xp, NOD, ER, T, EQ, Q)
+            E11, E22, _, mxm1, _, _, _, _, _, _ = deal_with_CS_VS_SUM_PLUS_SHAPE(xp, NOD, ER, T, EQ, Q)
         assert xp.NT >= 1 and xp.NT <= 9
 
         # REDUCTION
