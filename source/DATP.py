6d03c4d3b928c3dd7ff33f32430694d9c66da72d
gschnabel <40870991+gschnabel@users.noreply.github.com>
2025-03-18 06:33:41 +0100

    introduce `find_indices_with_tol` helper func

--- source/DATP.py
+++ source/DATP.py
@@ -114,6 +114,25 @@ def unflatten(flat_list, skeleton):
         return result
 
 
+def find_indices_with_tol(a, v, atol, rtol):
+    a = np.array(a)
+    v = np.array(v)
+    # bring into order
+    ordidcs = np.argsort(a)
+    a = a[ordidcs]
+    # match elements
+    idcs = np.minimum(np.searchsorted(a, v), len(a)-1)
+    s = np.isclose(a[idcs], v, atol=atol, rtol=rtol)
+    rem_idcs = np.maximum(idcs[~s]-1, 0)
+    idcs[~s] = rem_idcs
+    s[~s] = np.isclose(a[rem_idcs], v[~s], atol=atol, rtol=rtol)
+    idcs[~s] = -1  # indication for not found
+    # map to original order
+    found_sel = idcs != -1
+    idcs[found_sel] = ordidcs[idcs[found_sel]]
+    return idcs
+
+
 # global constants
 NQND = 'EB'
 NHEL = 'EL'
