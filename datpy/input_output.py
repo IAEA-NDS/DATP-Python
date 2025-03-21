8416c4ca6b41f46f3a65df002b309703d8fdf347
gschnabel <40870991+gschnabel@users.noreply.github.com>
2025-03-21 06:45:55 +0100

    remove unused var `NEB`

--- datpy/input_output.py
+++ datpy/input_output.py
@@ -142,10 +142,9 @@ def read_dataset(expdata_file_handle):
     # variables with local scope
     NAU: str; NREF: str; NQT: str
     NCOM: str; NQQ: str; NXQT: str
-    NXAU: str; NES: str; NEB: str
+    NXAU: str; NES: str;
 
     NES = 'ES'
-    NEB = END_DATA_BLOCK_INDICATION_STRING
 
     # this declaration is not present in Fortran code
     # but assumed to be implicitly done
