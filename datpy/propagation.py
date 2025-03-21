15e28cdfde56b9942cce9322e4c05dd501cf878a
gschnabel <40870991+gschnabel@users.noreply.github.com>
2025-03-21 09:26:41 +0100

    major refactoring: introduce `propagation` and `reduction` module

---
+++ datpy/propagation.py
@@ -0,0 +1,150 @@
+import numpy as np
+from .helpers import (
+    fort_write,
+    find_indices_with_tol,
+)
+from .constants import MAX_NUM_POINTS
+
+
+def deal_with_CS_and_CS_SHAPE(
+    dataset, prior_number_points, prior_energy_mesh, prior_cross_section
+):
+    EQ = np.empty((200,), dtype=float)
+    Q = np.zeros((MAX_NUM_POINTS,), dtype=float)
+    # CS + CS SHAPE
+    M1 = dataset.reaction_ids[0] - 1
+    NON = prior_number_points[M1]
+    NON1 = NON-1
+    E11 = (prior_energy_mesh[M1, 0] + prior_energy_mesh[M1, 1]) / 2.
+    E22 = (prior_energy_mesh[M1, NON-1] + prior_energy_mesh[M1, NON1-1]) / 2.
+    for K in range(NON):
+        EQ[K] = prior_energy_mesh[M1, K]
+        Q[K] = prior_cross_section[M1, K]
+    format4611 = "(i6,'  cr. sec. apriori for interp. ')"
+    fort_write(None, format4611, [NON-1])
+    return E11, E22, EQ, Q, NON-1
+
+
+def deal_with_RATIO_and_RATIO_SHAPE(
+    dataset, prior_number_points, prior_energy_mesh, prior_cross_section
+):
+    EQ = np.empty((200,), dtype=float)
+    Q = np.zeros((MAX_NUM_POINTS,), dtype=float)
+    # RATIO + RATIO SHAPE
+    M1 = dataset.reaction_ids[0] - 1
+    M2 = dataset.reaction_ids[1] - 1
+    NO1 = prior_number_points[M1]
+    NON = prior_number_points[M2]
+    mxm = 0
+
+    for K in range(NO1):
+        # find matching energies
+        L = find_indices_with_tol(prior_energy_mesh[M2,:NON], prior_energy_mesh[M1, K:K+1], atol=1e-8, rtol=1e-4).item()
+        if L == -1:  # not found
+            continue
+        EQ[mxm] = prior_energy_mesh[M1, K]
+        if prior_cross_section[M2, L] == 0.:
+            format4618 = ":(' apriori constr. ',4i5,f10.7,'*****************')"
+            fort_write(None, format4618, [M1-1, K+1, M2-1, L+1, EQ[mxm]])
+            exit()
+
+        Q[mxm] = prior_cross_section[M1, K] / prior_cross_section[M2, L]
+        mxm += 1
+
+    E11 = (EQ[0] + EQ[1]) / 2.
+    E22 = (EQ[mxm-1] + EQ[mxm-2]) / 2.
+
+    format4612 = "(i6,'  ratio apriori for interp. ')"
+    fort_write(None, format4612, [mxm])
+    return E11, E22, EQ, Q, mxm-1
+
+
+def deal_with_SUM_and_SHAPE_OF_SUM(
+    dataset, prior_number_points, prior_energy_mesh, prior_cross_section
+):
+    EQ = np.empty((200,), dtype=float)
+    Q = np.zeros((MAX_NUM_POINTS,), dtype=float)
+    # SUM AND SHAPE OF SUM
+    M1 = dataset.reaction_ids[0] - 1
+    M2 = dataset.reaction_ids[1] - 1
+    M3 = dataset.reaction_ids[2] - 1
+    NO1 = prior_number_points[M1]
+    NON = prior_number_points[M2]
+    if M3 != -1:
+        NO3 = prior_number_points[M3]
+
+    mxm = 0
+    for K in range(NO1):  # 23
+
+        L = find_indices_with_tol(
+            prior_energy_mesh[M2,:NON], prior_energy_mesh[M1, K:K+1], atol=1e-8, rtol=1e-4
+        ).item()
+        if L == -1:  # not found
+            continue
+
+        if M3 != -1:
+            J = find_indices_with_tol(
+                prior_energy_mesh[M3,:NO3], prior_energy_mesh[M1, K:K+1], atol=1e-8, rtol=1e-4
+            ).item()
+            if J == -1:  # not found
+                continue
+
+        EQ[mxm] = prior_energy_mesh[M1, K]
+        Q[mxm] = prior_cross_section[M1, K] + prior_cross_section[M2, L]
+        if M3 != -1:
+            Q[mxm] = Q[mxm] + prior_cross_section[M3, J]
+
+        mxm += 1 
+
+    E11 = (EQ[0] + EQ[1]) / 2.
+    E22 = (EQ[mxm-1] + EQ[mxm-2]) / 2.
+
+    format4613 = "(i6,'  sum apriori for interp. ')"
+    fort_write(None, format4613, [mxm])
+    return E11, E22, EQ, Q, mxm-1
+
+
+def deal_with_CS_VS_SUM_PLUS_SHAPE(
+    dataset, prior_number_points, prior_energy_mesh, prior_cross_section
+):
+    EQ = np.empty((200,), dtype=float)
+    Q = np.zeros((MAX_NUM_POINTS,), dtype=float)
+    #  RATIO OF CS VS. SUM + SHAPE
+    M1 = dataset.reaction_ids[0] - 1
+    M2 = dataset.reaction_ids[1] - 1
+    M3 = dataset.reaction_ids[2] - 1
+    NO1 = prior_number_points[M1]
+    NON = prior_number_points[M2]
+    NO3 = prior_number_points[M3]
+    mxm = 0
+    pymxm = mxm - 1
+
+    for K in range(NO1):
+
+        L = find_indices_with_tol(
+            prior_energy_mesh[M2,:NON], prior_energy_mesh[M1, K:K+1], atol=1e-8, rtol=1e-4
+        ).item()
+        if L == -1:  # not found
+            continue
+
+        if M3 != -1:
+            J = find_indices_with_tol(
+                prior_energy_mesh[M3,:NO3], prior_energy_mesh[M1, K:K+1], atol=1e-8, rtol=1e-4
+            ).item()
+            if J == -1:  # not found
+                continue
+
+        EQ[mxm] = prior_energy_mesh[M1, K]
+        Q[mxm] = (
+            prior_cross_section[M1, K] /
+            (prior_cross_section[M2, L] + prior_cross_section[M3, J])
+        )
+        mxm += 1
+
+    mxm1 = mxm - 1
+    E11 = (EQ[0] + EQ[1]) / 2.
+    E22 = (EQ[pymxm] + EQ[mxm1-1]) / 2.
+
+    format4614 = "(i6,'  cr. sec. vs sum apriori for interp. ')"
+    fort_write(None, format4614, [mxm])
+    return E11, E22, EQ, Q, mxm1
---
+++ datpy/reduction.py
@@ -0,0 +1,202 @@
+import numpy as np
+from copy import deepcopy
+from .helpers import fort_write
+from .constants import MAXF, ULI
+from .propagation import (
+    deal_with_CS_and_CS_SHAPE,
+    deal_with_RATIO_and_RATIO_SHAPE,
+    deal_with_SUM_and_SHAPE_OF_SUM,
+    deal_with_CS_VS_SUM_PLUS_SHAPE,
+)
+
+
+def reduce_dataset(
+    dataset, E11, E22, EQ, Q, mxm1, file_IO2
+):
+    dataset = deepcopy(dataset)
+    new_dataset = deepcopy(dataset)
+    #  Interpolation type (this is very system specific -
+    # does not apply for other simultaneous evaluations
+    interp_type = 'lin-lin'
+    if (dataset.quantity_type in (1, 2, 5, 8) and
+            dataset.reaction_ids[0] not in (2, 5, 10) and
+            dataset.reaction_ids[0] <= 10):
+        interp_type = 'log-log'
+
+    # GET GRID VALUES  - try at all apriori energies to find data
+    new_dataset.NO = 0
+    new_dataset.energies = np.zeros(mxm1-1, dtype=float)
+    new_dataset.measured_values = np.zeros(mxm1-1, dtype=float)
+
+    for L in range(1, mxm1):  # 40
+        E1 = (EQ[L-1] + EQ[L]) / 2.
+        E2 = (EQ[L] + EQ[L+1]) / 2.
+        E11 = 0.6 * EQ[L]
+        E22 = 1.4 * EQ[L]
+        if E1 < E11:
+            E1 = E11
+        if E2 > E22:
+            E2 = E22
+
+        AV = 0.
+        WTS = 0.
+        NKOT = 0
+        for N in range(12):  # 133
+            dataset.uncertainties[N, MAXF-1] = 0.
+
+        if E1 > .03:
+            interp_type = 'lin-lin'
+
+        # INTERPOLATION CONST.
+        if interp_type == 'lin-lin':
+            # LIN LIN
+            AL = (Q[L-1]-Q[L])/(EQ[L-1]-EQ[L])
+            BL = Q[L]-AL*EQ[L]
+            AR = (Q[L]-Q[L+1])/(EQ[L]-EQ[L+1])
+            BR = Q[L]-AR*EQ[L]
+        if interp_type == 'log-log':
+            # LOG LOG
+            QBL = (np.log(Q[L-1])-np.log(Q[L]))/(np.log(EQ[L])-np.log(EQ[L-1]))
+            QAL = Q[L]*(EQ[L]**QBL)
+            QBR = (np.log(Q[L])-np.log(Q[L+1]))/(np.log(EQ[L+1])-np.log(EQ[L]))
+            QAR = Q[L]*(EQ[L]**QBR)
+
+        # GRID VALUES
+        for K in range(dataset.NO):  # 35
+            if dataset.energies[K] < E1*(1.-1e-5) or dataset.energies[K] >= E2*(1.+1e-5):
+                continue
+
+            WT = 1./dataset.uncertainties[11, K]
+            WT = WT*WT
+            if dataset.energies[K] > EQ[L]:
+                # right of energy grid point
+                if interp_type == 'lin-lin':
+                    ADD = AR*dataset.energies[K] + BR
+                    AD = dataset.measured_values[K] + Q[L] - ADD
+                elif interp_type == 'log-log':
+                    ADD = QAR / (dataset.energies[K]**QBR)
+                    AD = dataset.measured_values[K] * Q[L] / ADD
+
+            elif dataset.energies[K] < EQ[L]:
+                # left o energy grid point
+                if interp_type == 'lin-lin':
+                    ADD = AL * dataset.energies[K] + BL
+                    AD = dataset.measured_values[K] + Q[L] - ADD
+                elif interp_type == 'log-log':
+                    ADD = QAL / (dataset.energies[K]**QBL)
+                    AD = dataset.measured_values[K] * Q[L] / ADD
+            else:
+                # same energy as grid point
+                AD = dataset.measured_values[K]
+
+            # check if difference is within requested limit of ULI*sigma
+            if ULI != 0:
+                T1X = 100.*(AD-Q[L])/Q[L]
+                T2X = T1X*T1X
+                TEST = np.sqrt(WT*T2X)
+                if TEST >= ULI:
+                    F33 = dataset.uncertainties[2, K] * dataset.uncertainties[2, K]
+                    F44 = 1./WT - F33
+                    FNEW = T2X / (ULI*ULI)
+                    F33N = FNEW - F44
+                    dataset.uncertainties[11, K] = np.sqrt(FNEW)
+                    dataset.uncertainties[2, K] = np.sqrt(F33N)
+                    WT = 1./FNEW
+
+                    format511 = "(20X,' VALUE OUTSIDE ',F5.2,' SIGMA BY ',F10.2)"
+                    fort_write(file_IO2, format511, [ULI, TEST])
+
+            AV = AV + AD*WT
+            WTS = WTS + WT
+
+            # statistical uncertainty reduces if more than one value contributes,
+            # average for all other uncertainties
+            for M in range(11):  # 38
+                if dataset.NETG[M] != 9:
+                    dataset.uncertainties[M, MAXF-1] = dataset.uncertainties[M, MAXF-1] + dataset.uncertainties[M, K]
+                elif dataset.uncertainties[M, K] != 0.0:
+                    dataset.uncertainties[M, MAXF-1] = dataset.uncertainties[M, MAXF-1] + (1./dataset.uncertainties[M, K])**2
+
+            NKOT = NKOT + 1
+
+        AKOT = NKOT
+        if AV != 0.0:
+            # GRID VALUE AND OUT
+            EEE = EQ[L]
+            QQQ = AV / WTS
+            DIF = QQQ / Q[L]
+            for N in range(11):  # 39
+                if dataset.NETG[N] != 9:
+                    dataset.uncertainties[N, MAXF-1] = dataset.uncertainties[N, MAXF-1] / AKOT
+                elif dataset.uncertainties[N, MAXF-1] > 0.0:
+                    dataset.uncertainties[N, MAXF-1] = 1. / np.sqrt(dataset.uncertainties[N, MAXF-1])
+                else:
+                    dataset.uncertainties[N, MAXF-1] = 0.
+
+            # OUTPUT
+            new_dataset.energies[new_dataset.NO] = EEE
+            new_dataset.measured_values[new_dataset.NO] = QQQ
+            new_dataset.uncertainties[0:12, new_dataset.NO] = dataset.uncertainties[0:12, MAXF-1]
+            new_dataset.NO += 1
+
+            if not hasattr(new_dataset, 'DIF'):
+                new_dataset.DIF = []
+            new_dataset.DIF.append(DIF)
+
+    new_dataset.uncertainties[0:12, MAXF-1] = dataset.uncertainties[0:12, MAXF-1]
+    return new_dataset
+
+
+def reduce_datablocks(datablocks, reaction_prior, file_IO2):
+    prior_number_points = reaction_prior['number_points']
+    prior_energy_mesh = reaction_prior['energy_mesh']
+    prior_cross_section = reaction_prior['cross_section']
+
+    reduced_datablocks = []
+    for datablock in datablocks:
+        reduced_datasets = []
+        for dataset in datablock:
+            # CONSTRUCT APRIORI
+
+            # NOTE: computed goto of fortran replaced
+            #       by if-else statements
+            if dataset.quantity_type in (1, 2):
+                E11, E22, EQ, Q, mxm1 = deal_with_CS_and_CS_SHAPE(
+                    dataset, prior_number_points, prior_energy_mesh, prior_cross_section
+                )
+            elif dataset.quantity_type in (3, 4):
+                E11, E22, EQ, Q, mxm1 = deal_with_RATIO_and_RATIO_SHAPE(
+                    dataset, prior_number_points, prior_energy_mesh, prior_cross_section
+                )
+            elif dataset.quantity_type in (5, 8):
+                E11, E22, EQ, Q, mxm1 = deal_with_SUM_and_SHAPE_OF_SUM(
+                    dataset, prior_number_points, prior_energy_mesh, prior_cross_section
+                )
+            elif dataset.quantity_type in (7, 9):
+                E11, E22, EQ, Q, mxm1 = deal_with_CS_VS_SUM_PLUS_SHAPE(
+                    dataset, prior_number_points, prior_energy_mesh, prior_cross_section
+                )
+            assert dataset.quantity_type >= 1 and dataset.quantity_type <= 9
+
+            # REDUCTION
+
+            if dataset.quantity_type != 6:
+                # skip datasets whose energies are byeond limits
+                if dataset.energies[0] > E22 or dataset.energies[dataset.NO-1] < E11:
+                    continue
+
+            if dataset.quantity_type != 6:
+                reduced_dataset = reduce_dataset(
+                    dataset, E11, E22, EQ, Q, mxm1, file_IO2
+                )
+            else:
+                # no reduction necessary for fission spectrum average dataset
+                reduced_dataset = deepcopy(dataset)
+
+            reduced_datasets.append(reduced_dataset)
+
+        if len(reduced_datasets) > 0:
+            reduced_datablocks.append(reduced_datasets)
+
+    return reduced_datablocks
+
