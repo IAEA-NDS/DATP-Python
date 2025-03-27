d35a672a7325493662bac15a50e500f83b2c8128
gschnabel <40870991+gschnabel@users.noreply.github.com>
2025-03-27 22:16:41 +0100

    start working on cli reduce functionality

--- datpy/extraction.py
+++ datpy/extraction.py
@@ -2,6 +2,7 @@ import os
 import io
 import sys
 import json
+from typing import Dict
 from .input_output import (
     copy_gma_controls,
 )
@@ -16,12 +17,14 @@ from .gma_output import (
 from .reduction import reduce_datablocks
 import argparse
 import logging
+from .datamodels.models import Dataset
+from .reduction import reduce_datablocks
 
 
 logger = logging.getLogger(__name__)
 
 
-def extract_datasets(gmdata_crd_content: str):
+def extract_datasets(gmdata_crd_content: str) -> Dict[int, dict]:
 
     datasets = {}
     fileobj = io.StringIO(gmdata_crd_content)
@@ -48,6 +51,16 @@ def extract_datasets(gmdata_crd_content: str):
     return datasets
 
 
+def reduce_datasets(datasets: Dict[int, dict]) -> dict:
+    reduced_datasets = {}
+    for dataset_id, raw_dataset in datasets.items():
+        dataset = Dataset(**raw_dataset)
+        if int(dataset_id) != dataset.dataset_id:
+            raise ValueError(f'Dataset {dataset.dataset_id} is stored under key {dataset_id}')
+        rd, _ = reduce_datablocks([[dataset]])
+        reduced_datasets[rd.dataset_id] = rd.dict()
+    return reduced_datasets
+
 
 if __name__ == '__main__':
 
@@ -56,13 +69,21 @@ if __name__ == '__main__':
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     )
 
+    stdin_cont = sys.stdin.read()
+
     parser = argparse.ArgumentParser()
-    parser.add_argument('filename')
+    subparsers = parser.add_subparsers(dest='command', help='Available commands')
+
+    extract_parser = subparsers.add_parser('extract', help='Extract data from GMDATA.CRD format')
+    reduce_parser = subparsers.add_parser('reduce', help='Reduce datasets')
 
     args = parser.parse_args()
-    fn = args.filename
-    with open(fn, 'r') as f:
-        cont = f.read()
 
-    datasets = extract_datasets(cont)
-    print(json.dumps(datasets, indent=2))
+    if args.command == 'extract':
+        datasets = extract_datasets(stdin_cont)
+        print(json.dumps(datasets, indent=2))
+    elif args.command == 'reduce':
+        datasets = json.loads(stdin_cont)
+        print(json.dumps(reduce_datasets(datasets), indent=2))
+    else:
+        raise ValueError('unknown command')
