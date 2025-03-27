497537fe8ef7a1c18a619c3b28928f8aaa6c04a3
gschnabel <40870991+gschnabel@users.noreply.github.com>
2025-03-27 21:07:27 +0100

    fix `use_arrays` argument usage

--- datpy/datamodels/models.py
+++ datpy/datamodels/models.py
@@ -6,8 +6,9 @@ class Dataset(DatasetBase):
 
     def dict(self, *args, use_arrays=False, **kwargs):
         model_dict = super().dict(*args, **kwargs)
+        if not use_arrays:
+            return model_dict
         for key, value in model_dict.items():
             if isinstance(value, list) and key not in ('comments',):
                 model_dict[key] = np.array(value)
-
         return model_dict
