8b667f23acd4af8b375425f4b9f5912859dee0d9
gschnabel <40870991+gschnabel@users.noreply.github.com>
2025-03-28 05:55:33 +0100

    make output optional in `copy_gma_controls` func

--- datpy/input_output.py
+++ datpy/input_output.py
@@ -9,23 +9,33 @@ from .constants import (
     DOWNWEIGHT_BLOCK_INDICATION_STRING,
     FISSION_SPECTRUM_BLOCK_INDICATION_STRING,
 )
+import logging
 
 
-def copy_gma_controls(prior_file_handle, file_IO2, gma_file_handle):
+logger = logging.getLogger(__name__)
+
+
+def _fort_write(file_handle, *args, **kwargs):
+    if file_handle is None:
+        return
+    return fort_write(file_handle, *args, **kwargs)
+
+
+def copy_gma_controls(prior_file_handle, file_IO2=None, gma_file_handle=None):
     for K in range(10):
         format260 = '(A2,A2,A1,8I5)'
-        format483 = "(' reading  ', a2)"
         KCO1, KCO2, KCO3, MC1, MC2, MC3, MC4, MC5, MC6, MC7, MC8 = \
             fort_read(prior_file_handle, format260, none_as=0.)
-        fort_write(None, format483, [KCO1])
+
+        logger.info(f'Reading control record {KCO1}')
 
         # exit loop if nothing more to read
         if KCO1.strip() == '':
             break
 
-        fort_write(gma_file_handle,  format260,
+        _fort_write(gma_file_handle,  format260,
                    [KCO1, KCO2, KCO3, MC1, MC2, MC3, MC4, MC5, MC6, MC7, MC8])
-        fort_write(file_IO2,  format260,
+        _fort_write(file_IO2,  format260,
                    [KCO1, KCO2, KCO3, MC1, MC2, MC3, MC4, MC5, MC6, MC7, MC8])
         if KCO1 == DOWNWEIGHT_BLOCK_INDICATION_STRING and MC2 == 10:
             # data set numbers selected for downweighting
@@ -33,8 +43,8 @@ def copy_gma_controls(prior_file_handle, file_IO2, gma_file_handle):
                 format402 = '16I5'
                 MSEP = np.empty((16,), dtype=int)
                 MSEP[:] = fort_read(prior_file_handle, format402)
-                fort_write(file_IO2, format402, [MSEP])
-                fort_write(gma_file_handle, format402, [MSEP])
+                _fort_write(file_IO2, format402, [MSEP])
+                _fort_write(gma_file_handle, format402, [MSEP])
                 if MSEP[0] == 0:
                     break
 
@@ -43,8 +53,8 @@ def copy_gma_controls(prior_file_handle, file_IO2, gma_file_handle):
             while True:
                 format404 = '(2E13.5)'
                 AE, BS = fort_read(prior_file_handle, format404, none_as=0.)
-                fort_write(file_IO2, format404, [AE, BS])
-                fort_write(gma_file_handle, format404, [AE, BS])
+                _fort_write(file_IO2, format404, [AE, BS])
+                _fort_write(gma_file_handle, format404, [AE, BS])
                 if AE == 0.0:
                     break
 
@@ -53,5 +63,5 @@ def copy_gma_controls(prior_file_handle, file_IO2, gma_file_handle):
             format468 = "('Data Sets to be Excluded')"
             NEXL = fort_read(prior_file_handle, format408)
             fort_write(gma_file_handle, format408, NEXL)
-            fort_write(file_IO2, format468, [None])
-            fort_write(file_IO2, format408, NEXL)
+            _fort_write(file_IO2, format468, [None])
+            _fort_write(file_IO2, format408, NEXL)
