16eeca4bf61f38670bec0d5c0bfcadd6ee217e36
gschnabel <40870991+gschnabel@users.noreply.github.com>
2025-03-29 08:20:10 +0100

    return spectrum from `copy_gma_controls` and add some type hints

--- datpy/input_output.py
+++ datpy/input_output.py
@@ -1,3 +1,4 @@
+from typing import TextIO, Optional
 import numpy as np
 import math
 from .helpers import (
@@ -21,7 +22,12 @@ def _fort_write(file_handle, *args, **kwargs):
     return fort_write(file_handle, *args, **kwargs)
 
 
-def copy_gma_controls(prior_file_handle, file_IO2=None, gma_file_handle=None):
+def copy_gma_controls(
+    prior_file_handle: TextIO,
+    file_IO2: Optional[TextIO]=None,
+    gma_file_handle: Optional[TextIO]=None
+) -> dict:
+    result = {}
     for K in range(10):
         format260 = '(A2,A2,A1,8I5)'
         KCO1, KCO2, KCO3, MC1, MC2, MC3, MC4, MC5, MC6, MC7, MC8 = \
@@ -50,6 +56,8 @@ def copy_gma_controls(prior_file_handle, file_IO2=None, gma_file_handle=None):
 
         elif KCO1 == FISSION_SPECTRUM_BLOCK_INDICATION_STRING and MC1 != 0:
             # fission spectrum
+            energies = []
+            spectrum_values = []
             while True:
                 format404 = '(2E13.5)'
                 AE, BS = fort_read(prior_file_handle, format404, none_as=0.)
@@ -57,6 +65,14 @@ def copy_gma_controls(prior_file_handle, file_IO2=None, gma_file_handle=None):
                 _fort_write(gma_file_handle, format404, [AE, BS])
                 if AE == 0.0:
                     break
+                energies.append(AE)
+                spectrum_values.append(BS)
+
+            result['spectrum'] = {
+                'label': 'spectrum',
+                'energies': energies,
+                'spectrum_values': spectrum_values,
+            }
 
         elif KCO1 == ELIMINATION_BLOCK_INDICATION_STRING:
             format408 = '(16i5)'
@@ -65,3 +81,4 @@ def copy_gma_controls(prior_file_handle, file_IO2=None, gma_file_handle=None):
             fort_write(gma_file_handle, format408, NEXL)
             _fort_write(file_IO2, format468, [None])
             _fort_write(file_IO2, format408, NEXL)
+    return result
