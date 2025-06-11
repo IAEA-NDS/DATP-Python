48df275487abded79870a12477bb645423c00b31
gschnabel <40870991+gschnabel@users.noreply.github.com>
2025-06-11 11:28:07 +0200

    implement `model_dump` method in `Dataset` class

--- datpy/datamodels/models.py
+++ datpy/datamodels/models.py
@@ -12,14 +12,21 @@ class ReactionPrior(ReactionPriorBase):
 
 class Dataset(DatasetBase):
 
-    def dict(self, *args, use_arrays=False, **kwargs):
-        model_dict = super().dict(*args, **kwargs)
+    def _model_dump(self, model_dict, use_arrays):
         if not use_arrays:
             return model_dict
         for key, value in model_dict.items():
             if isinstance(value, list) and key not in ('comments',):
                 model_dict[key] = np.array(value)
         return model_dict
+
+    def dict(self, *args, use_arrays=False, **kwargs):
+        model_dict = super().dict(*args, **kwargs)
+        return self._model_dump(model_dict, use_arrays)
+
+    def model_dump(self, *args, use_arrays=False, **kwargs):
+        model_dict = super().model_dump(*args, **kwargs)
+        return self._model_dump(model_dict, use_arrays)
 
 
 class FissionSpectrum(FissionSpectrumBase):
